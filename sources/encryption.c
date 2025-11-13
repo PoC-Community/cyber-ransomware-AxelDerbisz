4dc700f02e42d60be48a4328d09654427a09d074
Tokuuuu <72007811+iTokubz@users.noreply.github.com>
2025-11-13 19:10:37 +0100

    feat(encryption) add: completed encryption.c

--- sources/encryption.c
+++ sources/encryption.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CHUNK_SIZE 4096
+
 /*
 ** Here, you have to open both files with different permissions : think of what you want to
 ** to do with each file. Don't forget to check the return values of your syscalls !
@@ -11,7 +13,20 @@
 bool init_encryption(FILE **to_encrypt, FILE **encrypted,
     const char *filepath, const char *optfilepath)
 {
-    // step 2
+    *to_encrypt = fopen(filepath, "rb");
+    if (*to_encrypt == NULL) {
+        perror(filepath);
+        return false;
+    }
+
+    *encrypted = fopen(optfilepath, "wb");
+    if (*encrypted == NULL) {
+        perror(optfilepath);
+        fclose(*to_encrypt);
+        return false;
+    }
+
+    return true;
 }
 
 /*
@@ -21,7 +36,16 @@ bool init_encryption(FILE **to_encrypt, FILE **encrypted,
 int write_header(unsigned char *generated_key, FILE **to_encrypt,
     FILE **encrypted, crypto_secretstream_xchacha20poly1305_state *st)
 {
-    // step 2
+    unsigned char header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
+
+    crypto_secretstream_xchacha20poly1305_init_push(st, header, generated_key);
+
+    if (fwrite(header, 1, sizeof(header), *encrypted) != sizeof(header)) {
+        fprintf(stderr, "Error writing header to encrypted file\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 
 /*
@@ -33,5 +57,35 @@ int write_header(unsigned char *generated_key, FILE **to_encrypt,
 int encryption_loop(FILE *to_encrypt, FILE *encrypted,
     crypto_secretstream_xchacha20poly1305_state st)
 {
-    // step 2
+    unsigned char   in[CHUNK_SIZE];
+    unsigned char   out[CHUNK_SIZE + crypto_secretstream_xchacha20poly1305_ABYTES];
+    size_t          rlen;
+    unsigned long long out_len;
+    unsigned char   tag;
+
+    do {
+        rlen = fread(in, 1, CHUNK_SIZE, to_encrypt);
+
+        if (feof(to_encrypt)) {
+            tag = crypto_secretstream_xchacha20poly1305_TAG_FINAL;
+        } else {
+            tag = crypto_secretstream_xchacha20poly1305_TAG_MESSAGE;
+        }
+
+        crypto_secretstream_xchacha20poly1305_push(
+            &st,
+            out,
+            &out_len,
+            in,
+            rlen,
+            NULL, 0,
+            tag
+        );
+        if (fwrite(out, 1, out_len, encrypted) != out_len) {
+            fprintf(stderr, "Error writing encrypted chunk to file\n");
+            return EXIT_FAILURE;
+        }
+    } while (!feof(to_encrypt));
+
+    return EXIT_SUCCESS;
 }
